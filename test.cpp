95bad819af73bdfd33ec14291277ccf8af274f44
Max Comstock <mcomstock@g.hmc.edu>
2016-03-02 06:02:32 -0800

    The first version of testing is complete, and all tests pass.

--- test.cpp
+++ test.cpp
@@ -34,6 +34,43 @@ const int NUM_PARTICLES = 10;
 	add_n_particles_group(d, n-n/2, g2);		\
 	t = d.targetGroups(g1);
 
+// Macro for printing the elements of a vector, which we do frequently
+#define ITERATE_AND_PRINT(vect)					\
+	for (auto& element : vect) {				\
+		cout << element << " ";					\
+	}											\
+	cout << endl;
+
+// Macro for comparing two values. Print and return a failure state if they
+// are different.
+#define FAIL_IF_DIFFERENT(fst,snd) \
+	if (fst != snd) {			   \
+		cout << "FAILED" << endl;  \
+		return false;			   \
+	}
+
+// Macro for checking whether Diagram function fn affects groups and non-groups
+// with the same particles in the same way.
+#define COMPARE_TARGET_NOGROUP(groupVector,nogroupVector,fn)		\
+	t = d.targetGroups(red, blue);									\
+	Cell cn;														\
+	for (int i = 0; i < n; ++i) {									\
+		groupVector.clear();										\
+		nogroupVector.clear();										\
+																	\
+		c = d.getCell(i, t);										\
+		cn = d.getCell(i);											\
+		c.fn(groupVector);											\
+		cn.fn(nogroupVector);										\
+																	\
+		if (groupVector != nogroupVector) {							\
+			cout << "FAILED" << endl;								\
+			return false;											\
+		}															\
+	}																\
+	cout << "COMPLETE" << endl;
+
+// Macro for running a test and stating whether it passed or failed.
 #define TEST_PASS_OR_FAIL(name,fn)						\
 	if (fn) {											\
 		cout << name << " test complete" << endl;		\
@@ -97,10 +134,7 @@ bool neighbor_test_nogroups()
 		c.computeNeighbors(neighborList);
 
 		cout << "Neighbor list of particle " << i << ": ";
-		for (auto neighbor : neighborList) {
-			cout << neighbor << " ";
-		}
-		cout << endl;
+		ITERATE_AND_PRINT(neighborList);
 	}
 
 	return true;
@@ -147,10 +181,7 @@ bool vertices_test_nogroups()
 		c.computeVertices(verticesList);
 
 		cout << "Vertices of cell " << i << ": ";
-		for (auto vertex : verticesList) {
-			cout << vertex << " ";
-		}
-		cout << endl;
+		ITERATE_AND_PRINT(verticesList);
 	}
 
 	return true;
@@ -176,10 +207,7 @@ bool face_areas_test_nogroups()
 		c.computeFaceAreas(areaList);
 
 		cout << "Face areas of cell " << i << ": ";
-		for (auto area : areaList) {
-			cout << area << " ";
-		}
-		cout << endl;
+		ITERATE_AND_PRINT(areaList);
 	}
 
 	return true;
@@ -215,22 +243,13 @@ bool fake_cell_test_nogroups()
 	cout << "\tVolume: " << volume << endl;
 	
 	cout << "\tNeighbors: ";
-	for (auto neighbor : neighborList) {
-		cout << neighbor << " ";
-	}
-	cout << endl;
+	ITERATE_AND_PRINT(neighborList);
 
 	cout << "\tVertices: ";
-	for (auto vertex : verticesList) {
-		cout << vertex << " ";
-	}
-	cout << endl;
+	ITERATE_AND_PRINT(verticesList);
 
 	cout << "\tFace areas: ";
-	for (auto area : areaList) {
-		cout << area << " ";
-	}
-	cout << endl;
+	ITERATE_AND_PRINT(areaList);
 
 	return true;
 }
@@ -266,24 +285,226 @@ bool neighbor_test_targetgroups()
 		cout << endl;
 	}
 
-	cout << "Comparing neighbors with and without groups: ";
-	t = d.targetGroups(red, blue);
 	vector<int> neighborListNoGroups;
-	Cell cn;
+	cout << "Comparing neighbors with and without groups: ";
+	COMPARE_TARGET_NOGROUP(neighborList,neighborListNoGroups,computeNeighbors);
+
+	return true;
+}
+
+/*
+ * vertices_test_targetgroups
+ *
+ * Check that each cell can find its vertices when using target groups.
+ */
+bool vertices_test_targetgroups()
+{
+	int red, blue, n;
+	Diagram d;
+	TargetGroup t;
+	GROUP_HEADER(n,d,red,blue,t);
+
+	vector<double> verticesList;
+	Cell c;
 	for (int i = 0; i < n; ++i) {
-		neighborList.clear();
-		neighborListNoGroups.clear();
+		verticesList.clear();
 
 		c = d.getCell(i, t);
-		cn = d.getCell(i);
-		c.computeNeighbors(neighborList);
-		cn.computeNeighbors(neighborListNoGroups);
+		c.computeVertices(verticesList);
 
-		if (neighborList != neighborListNoGroups) {
-			cout << "FAILED" << endl;
-			return false;
-		}
+		cout << "Vertices of particle " << i << " in red group: ";
+		ITERATE_AND_PRINT(verticesList);
 	}
+
+	vector<double> verticesListNoGroups;
+	cout << "Comparing vertices with and without groups: ";
+	COMPARE_TARGET_NOGROUP(verticesList,verticesListNoGroups,computeVertices);
+
+	return true;
+}
+
+/*
+ * face_area_test_targetgroups
+ *
+ * Check that each cell can find its face areas when using target groups.
+ */
+bool face_area_test_targetgroups()
+{
+	int red, blue, n;
+	Diagram d;
+	TargetGroup t;
+	GROUP_HEADER(n,d,red,blue,t);
+
+	vector<double> areaList;
+	Cell c;
+	for (int i = 0; i < n; ++i) {
+		areaList.clear();
+
+		c = d.getCell(i, t);
+		c.computeFaceAreas(areaList);
+
+		cout << "Face areas of particle " << i << " in red group: ";
+		ITERATE_AND_PRINT(areaList);
+	}
+
+	vector<double> areaListNoGroups;
+	cout << "Comparing face areas with and without groups: ";
+	COMPARE_TARGET_NOGROUP(areaList,areaListNoGroups,computeFaceAreas);
+
+	return true;
+}
+
+/*
+ * volume_test_targetgroups
+ *
+ * Check that each cell can find its volume when using target groups.
+ */
+bool volume_test_targetgroups()
+{
+	int red, blue, n;
+	Diagram d;
+	TargetGroup t;
+	GROUP_HEADER(n,d,red,blue,t);
+
+	double volume;
+	Cell c;
+	for (int i = 0; i < n; ++i) {
+		c = d.getCell(i, t);
+		volume = c.computeVolume();
+
+		cout << "Volume of particle " << i << " in red group: " << volume << endl;
+	}
+
+	cout << "Comparing volumes with and without groups: ";
+	t = d.targetGroups(red, blue);
+	Cell cn;
+	for (int i = 0; i < n; ++i) {
+		c = d.getCell(i, t);
+		cn = d.getCell(i);
+
+		FAIL_IF_DIFFERENT(c.computeVolume(),cn.computeVolume());
+	}
+	cout << "COMPLETE" << endl;
+
+	return true;
+}
+
+/*
+ * fake_cell_test_targetgroups
+ *
+ * Check that a fake cell works correctly when using target groups.
+ */
+bool fake_cell_test_targetgroups()
+{
+	int red, blue, n;
+	Diagram d;
+	TargetGroup t;
+	GROUP_HEADER(n,d,red,blue,t);
+
+	double x = random_double();
+	double y = random_double();
+	double z = random_double();
+
+	Cell c = d.getCell(x, y, z, t);
+
+	vector<int> neighborList;
+	vector<double> verticesList;
+	vector<double> areaList;
+
+	double volume = c.computeVolume();
+	c.computeNeighbors(neighborList);
+	c.computeVertices(verticesList);
+	c.computeFaceAreas(areaList);
+
+	cout << "Computed fake particle at (" << x << ", " << y << ", " << z << ")" << endl;
+	cout << "\tVolume: " << volume << endl;
+	
+	cout << "\tNeighbors: ";
+	ITERATE_AND_PRINT(neighborList);
+
+	cout << "\tVertices: ";
+	ITERATE_AND_PRINT(verticesList);
+
+	cout << "\tFace areas: ";
+	ITERATE_AND_PRINT(areaList);
+
+	cout << "\tComparing fake cells with and without groups:" << endl;
+	t = d.targetGroups(red, blue);
+	c = d.getCell(x, y, z, t);
+	Cell cn = d.getCell(x, y, z);
+
+	neighborList.clear();
+	verticesList.clear();
+	areaList.clear();
+
+	volume = c.computeVolume();
+	c.computeNeighbors(neighborList);
+	c.computeVertices(verticesList);
+	c.computeFaceAreas(areaList);
+
+	vector<int> neighborListNG;
+	vector<double> verticesListNG;
+	vector<double> areaListNG;
+
+	double volumeNG = c.computeVolume();
+	cn.computeNeighbors(neighborListNG);
+	cn.computeVertices(verticesListNG);
+	cn.computeFaceAreas(areaListNG);
+
+	cout << "\t\tComparing volumes: ";
+	FAIL_IF_DIFFERENT(volume,volumeNG);
+	cout << "COMPLETE" << endl;
+
+	cout << "\t\tComparing neighbors: ";
+	FAIL_IF_DIFFERENT(neighborList,neighborListNG);
+	cout << "COMPLETE" << endl;
+
+	cout << "\t\tComparing vertices: ";
+	FAIL_IF_DIFFERENT(verticesList,verticesListNG);
+	cout << "COMPLETE" << endl;
+
+	cout << "\t\tComparing face areas: ";
+	FAIL_IF_DIFFERENT(areaList,areaListNG);
+	cout << "COMPLETE" << endl;
+
+	return true;
+}
+
+/*
+ * sourcegroup_test
+ *
+ * Check that source groups work correctly. Since this should just return a vector
+ * with specific indices, the test is minimal.
+ */
+bool sourcegroup_test()
+{
+	int red, blue, n;
+	Diagram d;
+	TargetGroup t;
+	GROUP_HEADER(n,d,red,blue,t);
+
+	vector<int> redVect;
+	vector<int> blueVect;
+	vector<int> all;
+	for (int i = 0; i < n/2; ++i) {
+		redVect.push_back(i);
+		all.push_back(i);
+	}
+	for (int i = n/2; i < n; ++i) {
+		blueVect.push_back(i);
+		all.push_back(i);
+	}
+
+	cout << "Checking correctness of red source group: ";
+	FAIL_IF_DIFFERENT(d.sourceGroups(red),redVect);
+	cout << "COMPLETE" << endl;
+
+	cout << "Checking correctness of blue source group: ";
+	FAIL_IF_DIFFERENT(d.sourceGroups(blue),blueVect);
+	cout << "COMPLETE" << endl;
+
+	cout << "Checking correctness of red and blue source group: ";
+	FAIL_IF_DIFFERENT(d.sourceGroups(red, blue),all);
 	cout << "COMPLETE" << endl;
 
 	return true;
@@ -313,6 +534,10 @@ bool run_nogroup_tests()
 bool run_targetgroup_tests()
 {
 	TEST_PASS_OR_FAIL("Neighbor",neighbor_test_targetgroups());
+	TEST_PASS_OR_FAIL("Vertices",vertices_test_targetgroups());
+	TEST_PASS_OR_FAIL("Face areas",face_area_test_targetgroups());
+	TEST_PASS_OR_FAIL("Volume",volume_test_targetgroups());
+	TEST_PASS_OR_FAIL("Fake cell",fake_cell_test_targetgroups());
 
 	return true;
 }
@@ -326,6 +551,7 @@ int main()
 {
 	TEST_PASS_OR_FAIL("No group",run_nogroup_tests());
 	TEST_PASS_OR_FAIL("Target group",run_targetgroup_tests());
+	TEST_PASS_OR_FAIL("Source group",sourcegroup_test());
 	
 	return 0;
 }
